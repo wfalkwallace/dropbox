9db84e030ef7abda71f8b1bb0126b37bebe1e429
William Falk-Wallace <wgf2104@columbia.edu>
2015-04-28 20:43:13 -0700

    adds basic server

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let express = require('express')
+let nodeify = require('bluebird-nodeify')
+let morgan = require('morgan')
+require('songbird')
+
+const NODE_ENV = process.env.NODE_ENV
+const PORT = process.env.PORT || 8000
+const ROOT_DIR = process.cwd()
+
 let app = express()
-app.listen(8000)
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+app.listen(PORT, () => console.log(`LISTENING AT http://127.0.0.1:${PORT}`))
+
+
